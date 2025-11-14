835e641cf542557ff444a8d09bef8c00332079d3
MatteoBucca <bucca.matteo@iisgalvanimi.edu.it>
2025-11-14 09:36:53 +0000

    ff

--- script.js
+++ script.js
@@ -2,11 +2,27 @@ function buttonFunction() {
   var canvas = document.getElementById("myCanvas");
   var ctx = canvas.getContext("2d");
   ctx.fillStyle = "#FF0000";
-  ctx.fillRect(0, 0, 150, 75);
+  ctx.fillRect(0, 0, 150, 75)
   ctx.fillStyle = "#00FF00";
 
   ctx.fillRect(0, 0, 10, 10);
-
   ctx.fillRect(20, 20, 10, 10);
+  
+  
+}
+function button2Function() {
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+  ctx.fillStyle = "#FF0000";
+  ctx.fillRect(0, 0, 150,75)
+  ctx.fillStyle = "#00FF00";
+
+  ctx.fillRect(0, 0, 10, 10);
+  ctx.fillRect(20, 20, 10, 10);
+  ctx.fillRect(40, 40, 10, 10);
+  ctx.fillRect(60, 60, 10, 10);
+  ctx.fillRect(80, 80, 10, 10);
+  ctx.fillRect(100, 100, 10, 10);
+  
 }
 
