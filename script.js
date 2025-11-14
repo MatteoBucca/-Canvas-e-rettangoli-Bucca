9da7135eb03f70b3691127b3356a08efddfa49ef
MatteoBucca <bucca.matteo@iisgalvanimi.edu.it>
2025-11-14 09:52:14 +0000

    canvas

--- script.js
+++ script.js
@@ -14,15 +14,17 @@ function button2Function() {
   var canvas = document.getElementById("myCanvas");
   var ctx = canvas.getContext("2d");
   ctx.fillStyle = "#FF0000";
-  ctx.fillRect(0, 0, 150,75)
+  ctx.fillRect(0, 0, 200,200)
   ctx.fillStyle = "#00FF00";
 
-  ctx.fillRect(0, 0, 10, 10);
-  ctx.fillRect(20, 20, 10, 10);
-  ctx.fillRect(40, 40, 10, 10);
-  ctx.fillRect(60, 60, 10, 10);
-  ctx.fillRect(80, 80, 10, 10);
-  ctx.fillRect(100, 100, 10, 10);
+ for (let i = 0; i <= 100; i += 20) {
+    ctx.fillRect(i, i, 10, 10);
+  }
+
   
+  for (let i = 0; i <= 100; i += 20) {
+    ctx.fillRect(100 - i, i, 10, 10);
+  }
 }
+
 
