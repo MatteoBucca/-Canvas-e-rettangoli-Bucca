2c78ef696c12210606b8236baf9129ec1f23761f
MatteoBucca <bucca.matteo@iisgalvanimi.edu.it>
2025-11-25 20:03:12 +0000

    modicicato3

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function buttonFunction() {
   var canvas = document.getElementById("myCanvas");
   var ctx = canvas.getContext("2d");
+   ctx.clearRect(0, 0, canvas.width, canvas.height); 
   ctx.fillStyle = "#FF0000";
   ctx.fillRect(0, 0, 150, 75)
   ctx.fillStyle = "#00FF00";
@@ -10,21 +11,51 @@ function buttonFunction() {
   
   
 }
+
 function button2Function() {
   var canvas = document.getElementById("myCanvas");
   var ctx = canvas.getContext("2d");
+   ctx.clearRect(0, 0, canvas.width, canvas.height); 
   ctx.fillStyle = "#FF0000";
   ctx.fillRect(0, 0, 200,200)
   ctx.fillStyle = "#00FF00";
 
- for (let i = 0; i <= 100; i += 20) {
+ for (let i = 0; i <= 180; i += 20) {
     ctx.fillRect(i, i, 10, 10);
   }
 
   
-  for (let i = 0; i <= 100; i += 20) {
-    ctx.fillRect(100 - i, i, 10, 10);
+  for (let i = 0; i <= 180; i += 20) {
+   
+    ctx.fillRect(190- i, i, 10, 10);
+    
   }
+ 
+}
+function button3Function() {
+    const canvas = document.getElementById("myCanvas");
+    const ctx = canvas.getContext("2d");
+
+    ctx.fillStyle = "blue";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    const lato = 10;   // lato quadratino
+    const passo = 20;  // distanza tra quadratini
+
+    ctx.fillStyle = "dodgerblue";
+
+    for (let y = 20; y < canvas.height - 20; y += passo) {
+        for (let x = 20; x < canvas.width - 20; x += passo) {
+            ctx.fillRect(x, y, lato, lato);
+        }
+    }
+    ctx.fillStyle = "red";
+    for (let i = 0; i < canvas.height - 20; i += 20) {
+   
+    ctx.fillRect(190- i, i, 10, 10);
+    
+  }
+ 
 }
 
 
