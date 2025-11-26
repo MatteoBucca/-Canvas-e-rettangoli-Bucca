125179ad5705ecacbea90aeff5ba163c1d88da0a
MatteoBucca <bucca.matteo@iisgalvanimi.edu.it>
2025-11-26 19:56:59 +0000

    modificato26112025

--- script.js
+++ script.js
@@ -44,18 +44,59 @@ function button3Function() {
 
     ctx.fillStyle = "dodgerblue";
 
-    for (let y = 20; y < canvas.height - 20; y += passo) {
-        for (let x = 20; x < canvas.width - 20; x += passo) {
+    for (let y = 0; y < canvas.height ; y += passo) {
+        for (let x = 0; x < canvas.width ; x += passo) {
             ctx.fillRect(x, y, lato, lato);
         }
     }
     ctx.fillStyle = "red";
-    for (let i = 0; i < canvas.height - 20; i += 20) {
+    for (let i = 0; i < canvas.height; i += 20) {
    
     ctx.fillRect(190- i, i, 10, 10);
     
   }
+
  
 }
+function button4Function(){
+  const canvas = document.getElementById("myCanvas");
+  const ctx = canvas.getContext("2d");
+
+  const cellSize = 10;
+  const step = 20; 
+
+  const rows = canvas.height / step;
+  const cols = canvas.width / step;
 
 
+  ctx.fillStyle = "#00ff37ff";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  
+  ctx.fillStyle = "#2600ffff";
+
+  for (let r = 0; r < 200; r+=20) {
+    for (let c = 0; c < 200; c+=20) {
+
+      
+      if (r + c >= 200 - 1) continue;
+
+      ctx.fillRect(c , r , cellSize, cellSize);
+    }
+  }
+
+  // ---- DIAGONALE GIALLA ----
+  ctx.fillStyle = "#e1ff00ff";
+  for (let i = 0; i < 200;  i += 20) {
+    ctx.fillRect(190- i, i, 10, 10);
+  }
+  ctx.fillStyle = "#0011ffff";
+  for (let i = 0; i < 200;  i += 20) {
+    ctx.fillRect(200- i, i, 10, 10);
+  }
+  ctx.fillStyle = "#e1ff00ff";
+  for (let i = 0; i <= 80; i += 20) {
+    ctx.fillRect(i, i, 10, 10);
+  }
+
+}
